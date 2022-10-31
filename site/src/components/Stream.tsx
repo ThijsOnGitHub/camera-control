2fbb9821e033c8509159e2e9e640d120480904fc
ThijsOnGitHub <47325246+ThijsOnGitHup@users.noreply.github.com>
2022-10-31 20:15:17 +0100

    Add stream frontend

--- site/src/components/Stream.tsx
+++ site/src/components/Stream.tsx
@@ -1,26 +1,36 @@
 import {createElement, createRef, FC, forwardRef, RefObject, useEffect, useRef} from "react";
 const Wfs = require("./wfs.js");
+import { loadPlayer } from 'rtsp-relay/browser';
+
 
 export const Stream: FC<{ip:string}> = (props)=>{
-    const video = createRef<HTMLVideoElement>()
+    const videoCanvas = createRef<HTMLCanvasElement>()
     const canvas = createRef<HTMLCanvasElement>()
 
     const takeScreenShot = () => {
-        if(!video.current || !canvas.current) return
+        if(!videoCanvas.current || !canvas.current) return
         const canvasTemp = createElement("canvas") as unknown as HTMLCanvasElement
         let ctx = canvasTemp.getContext('2d');
         if(!ctx) return
-        ctx.drawImage( video.current, 0, 0, canvasTemp.width, canvasTemp.height );
+        ctx.drawImage( videoCanvas.current, 0, 0, canvasTemp.width, canvasTemp.height );
     }
 
+
+
     useEffect(()=>{
-        // @ts-ignore
-        const wfs = new Wfs()
-        wfs.attachMedia(video.current, `ws://${props.ip}:8088/live/av0`);
+        if(videoCanvas.current){
+            loadPlayer({
+                url: `ws://localhost:4123/api/stream/${props.ip}`,
+                canvas: videoCanvas.current,
+                // optional
+                onDisconnect: () => console.log('Connection lost!'),
+            });
+        }
+
     })
 
     return <div className={'button-video-container'}>
-        <video className={'button-video'} ref={video}></video>
+        <canvas className={'button-video'} ref={videoCanvas}></canvas>
     </div>
 }
 
