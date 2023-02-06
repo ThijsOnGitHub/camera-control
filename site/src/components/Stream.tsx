21444953528170f15473d536d9a6165e44bc5244
ThijsOnGitHub <47325246+ThijsOnGitHup@users.noreply.github.com>
2023-02-06 22:06:13 +0100

    Fix loading start player

--- site/src/components/Stream.tsx
+++ site/src/components/Stream.tsx
@@ -1,6 +1,6 @@
-import {createElement, createRef, FC, forwardRef, RefObject, useCallback, useEffect, useRef} from "react";
-const Wfs = require("./wfs.js");
-import { loadPlayer } from 'rtsp-relay/browser';
+import {createRef, FC, useEffect} from "react";
+import { Player} from 'rtsp-relay/browser';
+import {PlayerOptions} from "rtsp-relay/browser/jsmpeg";
 
 export type ScreenShotFunctie = HTMLCanvasElement
 
@@ -11,18 +11,93 @@ export const Stream: FC<{ip:string,setTakeScreenshot?: (screenshotFunction:Scree
             return videoCanvas.current?.toDataURL('image/png');
     }
 
+    const importJSMpeg = () =>
+        new Promise((resolve, reject) => {
+            const script = Object.assign(document.createElement('script'), {
+                src: 'jsmpeg.min.js',
+                onload: resolve,
+                onerror: reject,
+            });
+            document.head.appendChild(script);
+        });
+
+    /**
+     * Creates a `Player`. If you intend to create multiple players, you must
+     * await for this promise to complete before creating the next player.
+     * @param {import("./jsmpeg").PlayerOptions} options
+     * @returns {Promise<Player>}
+     */
+    const loadPlayer: (options: PlayerOptions)=> Promise<Player>  = ({
+                            url,
+                            onDisconnect,
+                            disconnectThreshold = 3e3,
+                            ...options
+                        }) =>
+        importJSMpeg().then(() => {
+            return new Promise((resolve, reject) => {
+                // hide the canvas until it's loaded and the correct size
+                const originalDisplay = options.canvas.style.display;
+                // eslint-disable-next-line no-param-reassign
+                options.canvas.style.display = 'none';
+
+                let lastRx = Date.now(); // Date.now() is more efficient than performance.now()
+                if (options.onVideoDecode && onDisconnect) {
+                    reject(
+                        new Error('You cannot specify both onDisconnect and onVideoDecode'),
+                    );
+                    return;
+                }
+
+                const player = new window.JSMpeg.Player(url, {
+                    // for performance reasons, only record last packet Rx time if onDisconnect is specified
+                    onVideoDecode: onDisconnect
+                        ? () => {
+                            lastRx = Date.now();
+                        }
+                        : undefined,
+                    ...options,
+                });
+
+                const o = new MutationObserver((mutations) => {
+                    if (mutations.some((m) => m.type === 'attributes')) {
+                        // eslint-disable-next-line no-param-reassign
+                        options.canvas.style.display = originalDisplay;
+                        resolve(player);
+                        o.disconnect();
+                    }
+                });
+                o.observe(options.canvas, { attributes: true });
+
+                if (onDisconnect) {
+                    const i = setInterval(() => {
+                        if (Date.now() - lastRx > disconnectThreshold) {
+                            onDisconnect();
+                            clearInterval(i);
+                        }
+                    }, disconnectThreshold / 2);
+                }
+            });
+        });
+
+    async function startVideoPreview() {
+        if(videoCanvas.current){
+            const canvas = videoCanvas.current
+            setTimeout(()=>{
+                loadPlayer({
+                    url: `ws://localhost:4123/api/stream/${props.ip}`,
+                    canvas: canvas,
+                    disableGl: true,
+                    disconnectThreshold: 10000,
+                    // optional
+                    onDisconnect: () => console.log('Connection lost!'),
+                });
+            },1000)
+        }
+    }
 
     useEffect(()=>{
-        if(videoCanvas.current && props.setTakeScreenshot ) props.setTakeScreenshot(videoCanvas.current)
-        if(videoCanvas.current){
-            loadPlayer({
-                url: `ws://localhost:4123/api/stream/${props.ip}`,
-                canvas: videoCanvas.current,
-                disableGl: true,
-                // optional
-                onDisconnect: () => console.log('Connection lost!'),
-            });
-        }
+            if(videoCanvas.current && props.setTakeScreenshot ) props.setTakeScreenshot(videoCanvas.current)
+            startVideoPreview()
     },[videoCanvas.current])
 
     return <div className={'button-video-container'}>
